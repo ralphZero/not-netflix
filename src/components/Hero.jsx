c6e25464b4ac5412391a6fe3da1ec268de87862c
ralphZero <placideralph@gmail.com>
2022-08-10 20:09:49 -0400

    importing custom font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './Hero.css';
 
 const Hero = () => {
     return (
         <Container className='hero d-flex flex-column'>
-            <Container className='flex-grow-1 bg-warning'>
-                Hero text and call to actions
+            <Container className='flex-grow-1 bg-warning p-0'>
+            <Row className='h-100'>
+                <Col xs={12} md={7} xl={6}>
+                    <Container className='bg-success h-100 d-flex align-items-center'>
+                        <h1 className='display-1 bolder'>Thor: Love and Thunder</h1>
+                    </Container>
+                </Col>
+            </Row>
             </Container>
             <Container className='bg-primary'>
                 Movie cards
