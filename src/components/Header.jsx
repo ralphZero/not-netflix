243a3f552c8a2135a2919945516e5ba5dd27c3b2
ralphZero <placideralph@gmail.com>
2022-08-11 20:11:37 -0400

    adding shadow to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
-        <Navbar fixed='top' bg="dark" className="bg-transparent" variant="dark">
+        <Navbar fixed='top' bg="dark" className="bg-transparent bgshadow" variant="dark">
             <Container className="px-xs-2 px-md-4 px-lg-5" fluid>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <img src={logo} height={30} alt="logo" />
+            </Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#home">Movies</Nav.Link>
+                <Nav.Link href="#features">TV Shows</Nav.Link>
             </Nav>
             </Container>
         </Navbar>
